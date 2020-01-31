6ed1e584261293ea60163271b064374c27520bde
BuildTools <unconfigured@null.spigotmc.org>
2020-01-31 13:37:51 +0100

    some website file system tests #3

--- index.js
+++ index.js
@@ -108,14 +108,34 @@ SynTexPlatform.prototype = {
                 }
                 else if(urlPath == '/test')
                 {
-                    fs.readFile(__dirname + '/test.php', function (err, data)
-                    {
-                        if(err)
-                        {
-                            throw err; 
+                    let pathname = path.join(__dirname, "index.php");
+
+                    fs.exists(pathname, function (exist) {
+                        if(!exist) {
+                            // if the file is not found, return 404
+                            res.statusCode = 404;
+                            res.end(`File ${pathname} not found!`);
+                            return;
                         }
-                        
-                        response.write(data.toString());
+
+                        // if is a directory, then look for index.html
+                        if (fs.statSync(pathname).isDirectory()) {
+                            pathname += '/index.html';
+                        }
+
+                        // read file from file system
+                        fs.readFile(pathname, function(err, data){
+                            if(err){
+                                res.statusCode = 500;
+                                res.end(`Error getting the file: ${err}.`);
+                            } else {
+                                // based on the URL path, extract the file extention. e.g. .js, .doc, ...
+                                const ext = path.parse(pathname).ext;
+                                // if the file is found, set Content-type and send data
+                                response.setHeader('Content-type', 'text/plain');
+                                response.end(data);
+                            }
+                        });
                     });
                 }
                 else if(urlPath == '/ping')
