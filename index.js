735986c6fbf92589682b6afa911f616bbd6cd343
SynTexDZN <syntexdzn@t-online.de>
2020-02-07 22:03:20 +0000

    fixed head loading issue

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var request = require('request');
+//var request = require('request');
 var http = require('http');
 var url = require('url');
 var fs = require('fs');
@@ -39,194 +39,196 @@ SynTexPlatform.prototype = {
             var urlPath = urlParts.pathname;
             var body = [];
             
-            request.on('error', (function(err)
+            body = Buffer.concat(body).toString();
+
+            response.statusCode = 200;
+            response.setHeader('Content-Type', 'application/json');
+            response.setHeader('Access-Control-Allow-Origin', 'http://syntex.local');
+
+            if(urlPath == '/init')
             {
-                log('\x1b[31m%s\x1b[0m', "[ERROR]", "Reason: ", err);
-                
-            }).bind(this)).on('data', function(chunk)
-            {
-                body.push(chunk);
-                
-            }).on('end', (function()
-            {
-                body = Buffer.concat(body).toString();
-
-                response.statusCode = 200;
-                response.setHeader('Content-Type', 'application/json');
-                response.setHeader('Access-Control-Allow-Origin', 'http://syntex.local');
-                
-                if(urlPath == '/init')
+                if(urlParams.name && urlParams.type && urlParams.mac && urlParams.ip && urlParams.version && urlParams.refresh)
                 {
-                    if(urlParams.name && urlParams.type && urlParams.mac && urlParams.ip && urlParams.version && urlParams.refresh)
-                    {
-                        DeviceManager.initDevice(urlParams.mac, urlParams.ip, urlParams.name, urlParams.type, urlParams.version, urlParams.refresh).then(function(res) {
+                    DeviceManager.initDevice(urlParams.mac, urlParams.ip, urlParams.name, urlParams.type, urlParams.version, urlParams.refresh).then(function(res) {
 
-                            response.write(res[1]);
-                            response.end();
+                        response.write(res[1]);
+                        response.end();
 
-                            if(res[0] == "Init")
-                            {
-                                const { exec } = require("child_process");
-
-                                exec("sudo systemctl restart homebridge", (error, stdout, stderr) => {
-                                    log('\x1b[31m%s\x1b[0m', "[WARNING]", "Die Homebridge wird neu gestartet ..");
-                                });
-                            }
-                        });
-                    }
-                }
-                else if(urlPath == '/remove-device')
-                {
-                    if(urlParams.mac && urlParams.type)
-                    {
-                        DeviceManager.removeDevice(urlParams.mac, urlParams.type).then(function(res) {
-                        
-                            if(res)
-                            {
-                                response.write("Success");
-                                response.end(); 
-
-                                const { exec } = require("child_process");
-
-                                exec("sudo systemctl restart homebridge", (error, stdout, stderr) => {
-                                    log('\x1b[31m%s\x1b[0m', "[WARNING]", "Die Homebridge wird neu gestartet ..");
-                                });
-                            }
-                            else
-                            {
-                                response.write("Das Gerät konnte nicht entfernt werden!");
-                                response.end();
-                            }
-                        });
-                    }
-                }
-                else if(urlPath == '/ping')
-                {
-                    response.write("");
-                    response.end();
-                }
-                else
-                {
-                    var pathname = path.join(__dirname, urlPath.substring(1));
-                    
-                    var noext = false;
-                    
-                    if(path.parse(urlPath).ext == '')
-                    {
-                        noext = true;
-                    }
-  
-                    fs.exists(pathname, function(exist)
-                    {
-                        if(exist || noext)
+                        if(res[0] == "Init")
                         {
-                            if(exist && fs.statSync(pathname).isDirectory())
-                            {
-                                pathname += '/index.html';
-                            }
-                            else if(noext)
-                            {
-                                pathname += '.html';
-                            }
+                            const { exec } = require("child_process");
 
-                            fs.readFile(pathname, function(err, data)
-                            {
-                                if(err)
-                                {
-                                    response.statusCode = 500;
-                                    response.end('Die Seite konnte nicht geladen werden: ' + err);
-                                }
-                                else
-                                {
-                                    fs.readFile(__dirname + '/includes/head.html', function(err, head)
-                                    {                                        
-                                        if(!head)
-                                        {
-                                            head = "";
-                                        }
-                                        
-                                        var mimeType = {
-                                            ".html": "text/html",
-                                            ".jpeg": "image/jpeg",
-                                            ".jpg": "image/jpeg",
-                                            ".png": "image/png",
-                                            ".js": "text/javascript",
-                                            ".css": "text/css",
-                                            ".ttf": "font/ttf"
-                                        };
+                            exec("sudo systemctl restart homebridge", (error, stdout, stderr) => {
+                                log('\x1b[31m%s\x1b[0m', "[WARNING]", "Die Homebridge wird neu gestartet ..");
+                            });
+                        }
+                    });
+                }
+            }
+            else if(urlPath == '/remove-device')
+            {
+                if(urlParams.mac && urlParams.type)
+                {
+                    DeviceManager.removeDevice(urlParams.mac, urlParams.type).then(function(res) {
 
-                                        log(urlPath);
-                                        log(urlPath.startsWith('/index'));
+                        if(res)
+                        {
+                            response.write("Success");
+                            response.end(); 
 
-                                        response.setHeader('Content-Type', mimeType[path.parse(urlPath).ext] || 'text/html; charset=utf-8');
+                            const { exec } = require("child_process");
 
-                                        if(urlPath.startsWith('/devices/') && urlParams.mac)
-                                        {
-                                            DeviceManager.getDevice(urlParams.mac).then(function(res) {
-
-                                                log(res);
-
-                                                response.write(HTMLQuery.send(head + data, 'device', JSON.stringify(res)));
-                                                response.end();
-                                            });
-                                        }
-                                        else if(urlPath == '/' || urlPath.startsWith('/index') || urlPath.startsWith('/settings'))
-                                        {
-                                            DeviceManager.getDevices().then(function(res) {
-
-                                                log(res);
-
-                                                response.write(HTMLQuery.send(head + data, 'devices', JSON.stringify(res)));
-                                                response.end();
-                                            });
-                                        }
-                                        if(urlPath.startsWith('/bridge'))
-                                        {
-                                            var pjson = require('./package.json');
-                                            var ifaces = require('os').networkInterfaces();
-                                            var address;
-                                            
-                                            for (var dev in ifaces)
-                                            {
-                                                var iface = ifaces[dev].filter(function(details)
-                                                {
-                                                    return details.family === 'IPv4' && details.internal === false;
-                                                });
-
-                                                if(iface.length > 0) address = iface[0].address;
-                                            }
-                                            
-                                            var obj = {
-                                                ip: address,
-                                                version: pjson.version
-                                            };
-                                            
-                                            response.write(HTMLQuery.send(head + data, obj));
-                                            response.end();
-                                        }
-                                        else if(path.parse(urlPath).ext == '.html')
-                                        {
-                                            response.write(head + data);
-                                            response.end();
-                                        }
-                                        else
-                                        {
-                                            response.write(data);
-                                            response.end();
-                                        }
-                                    });
-                                }
+                            exec("sudo systemctl restart homebridge", (error, stdout, stderr) => {
+                                log('\x1b[31m%s\x1b[0m', "[WARNING]", "Die Homebridge wird neu gestartet ..");
                             });
                         }
                         else
                         {
-                            response.statusCode = 404;
-                            response.end('Die Seite ' + pathname + ' wurde nicht gefunden!');
+                            response.write("Das Gerät konnte nicht entfernt werden!");
+                            response.end();
                         }
                     });
                 }
-                
-            }).bind(this));
+            }
+            else if(urlPath == '/ping')
+            {
+                response.write("");
+                response.end();
+            }
+            else
+            {
+                var pathname = path.join(__dirname, urlPath.substring(1));
+
+                var noext = false;
+
+                if(path.parse(urlPath).ext == '')
+                {
+                    noext = true;
+                }
+
+                fs.exists(pathname, function(exist)
+                {
+                    if(exist || noext)
+                    {
+                        if(exist && fs.statSync(pathname).isDirectory())
+                        {
+                            pathname += '/index.html';
+                        }
+                        else if(noext)
+                        {
+                            pathname += '.html';
+                        }
+                        
+                        log('Pathname', pathname);
+
+                        fs.readFile(pathname, function(err, data)
+                        {
+                            if(err)
+                            {
+                                response.statusCode = 500;
+                                response.end('Die Seite konnte nicht geladen werden: ' + err);
+                            }
+                            else
+                            {
+                                log('dirname', __dirname + '/includes/head.html');
+                                
+                                fs.readFile(__dirname + '/includes/head.html', function(err, head)
+                                {                                        
+                                    if(!head)
+                                    {
+                                        head = "";
+                                    }
+                                    
+                                    //log('head', head.toString())
+
+                                    var mimeType = {
+                                        ".html": "text/html",
+                                        ".jpeg": "image/jpeg",
+                                        ".jpg": "image/jpeg",
+                                        ".png": "image/png",
+                                        ".js": "text/javascript",
+                                        ".css": "text/css",
+                                        ".ttf": "font/ttf"
+                                    };
+
+                                    log(urlPath);
+                                    log(urlPath.startsWith('/index'));
+
+                                    response.setHeader('Content-Type', mimeType[path.parse(urlPath).ext] || 'text/html');
+
+                                    if(urlPath.startsWith('/devices/') && urlParams.mac)
+                                    {
+                                        DeviceManager.getDevice(urlParams.mac).then(function(res) {
+
+                                            //log(res);
+
+                                            log(4);
+                                            
+                                            response.write(HTMLQuery.send(head + data, 'device', JSON.stringify(res)));
+                                            response.end();
+                                        });
+                                    }
+                                    else if(urlPath == '/' || urlPath.startsWith('/index') || urlPath.startsWith('/settings'))
+                                    {
+                                        log(3);
+                                        
+                                        DeviceManager.getDevices().then(function(res) {
+
+                                            //log(res);
+
+                                            response.end(HTMLQuery.send(head + data, 'devices', JSON.stringify(res)));
+                                            response.end();
+                                        });
+                                    }
+                                    else if(urlPath.startsWith('/bridge'))
+                                    {
+                                        var pjson = require('./package.json');
+                                        var ifaces = require('os').networkInterfaces();
+                                        var address;
+
+                                        for (var dev in ifaces)
+                                        {
+                                            var iface = ifaces[dev].filter(function(details)
+                                            {
+                                                return details.family === 'IPv4' && details.internal === false;
+                                            });
+
+                                            if(iface.length > 0) address = iface[0].address;
+                                        }
+
+                                        var obj = {
+                                            ip: address,
+                                            version: pjson.version
+                                        };
+
+                                        response.write(HTMLQuery.send(head + data, obj));
+                                        response.end();
+                                    }
+                                    else if(path.parse(urlPath).ext == '.html')
+                                    {
+                                        log(2);
+                                        
+                                        response.write(head + data);
+                                        response.end();
+                                    }
+                                    else
+                                    {
+                                        log(1);
+                                        
+                                        response.write(data);
+                                        response.end();
+                                    }
+                                });
+                            }
+                        });
+                    }
+                    else
+                    {
+                        response.statusCode = 404;
+                        response.end('Die Seite ' + pathname + ' wurde nicht gefunden!');
+                    }
+                });
+            }
             
         }).bind(this);
 
