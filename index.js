2db85d12d26d2a8bd269d3a62d34d26f7b206bc9
SynTexDZN <syntexdzn@t-online.de>
2020-02-10 00:44:01 +0000

    minor fixes

--- index.js
+++ index.js
@@ -92,21 +92,12 @@ SynTexPlatform.prototype = {
             {
                 const { exec } = require("child_process");
                 
+                response.write('Success');
+                response.end();
+                
                 exec("sudo systemctl restart homebridge", (error, stdout, stderr) => {
 
                     log('\x1b[31m%s\x1b[0m', "[WARNING]", "Die Homebridge wird neu gestartet ..");
-                    
-                    if(error || stderr.includes('ERR!'))
-                    {
-                        response.write('Error');
-                    }
-                    else
-                    {
-                        response.write('Success');
-                    }
-                    
-                    response.end();
-                    
                 });
             }
             else if(urlPath == '/update')
