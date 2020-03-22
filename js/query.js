b4ed941d84986a097387ffa33f4b976ca7814376
SynTexDZN <syntexdzn@t-online.de>
2020-03-22 11:57:05 +0100

    new query system check

---
+++ js/query.js
@@ -0,0 +1,86 @@
+async function fetch(url)
+{
+    return new Promise(resolve => {
+        
+        var client = new XMLHttpRequest();
+
+        client.timeout = 10000;
+
+        client.open('GET', url);
+
+        client.onreadystatechange = function()
+        {
+            if(client.readyState === 4)
+            {  
+                if(client.status === 200)
+                {
+                    resolve(client.responseText);
+                }
+                else
+                {
+                    resolve(false);
+                }
+            }
+        }
+
+        client.send();
+    });
+}
+
+async function fetch(url, timeout)
+{
+    return new Promise(resolve => {
+        
+        var client = new XMLHttpRequest();
+
+        client.timeout = timeout;
+
+        client.open('GET', url);
+
+        client.onreadystatechange = function()
+        {
+            if(client.readyState === 4)
+            {  
+                if(client.status === 200)
+                {
+                    resolve(client.responseText);
+                }
+                else
+                {
+                    resolve(false);
+                }
+            }
+        }
+
+        client.send();
+    });
+}
+
+async function fetch(url, timeout, post)
+{
+    return new Promise(resolve => {
+        
+        var client = new XMLHttpRequest();
+
+        client.timeout = timeout;
+
+        client.open('POST', url);
+
+        client.onreadystatechange = function()
+        {
+            if(client.readyState === 4)
+            {  
+                if(client.status === 200)
+                {
+                    resolve(client.responseText);
+                }
+                else
+                {
+                    resolve(false);
+                }
+            }
+        }
+
+        client.send(post);
+    });
+}
