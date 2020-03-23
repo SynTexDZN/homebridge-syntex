316d1de52ef1acaabcee2d60f773a6a81639122a
SynTexDZN <syntexdzn@t-online.de>
2020-03-23 15:48:15 +0100

    tests

--- js/loading-loop.js
+++ js/loading-loop.js
@@ -4,7 +4,9 @@ function searchLoop()
 
     for(var i = 0; i < loop.length; i++)
     {
-        if(loop[i].hasAttribute('value') && loop[i].value.substring(loop[i].value.length - 3) == ' ..')
+        if(loop[i].hasAttribute('value') && loop[i].value.substring(loop[i].value.length - 2) == ' .'
+        || loop[i].hasAttribute('value') && loop[i].value.substring(loop[i].value.length - 3) == ' ..'
+        || loop[i].hasAttribute('value') && loop[i].value.substring(loop[i].value.length - 4) == ' ...')
         {
             if((loop[i].value.match(/\./g) || []).length < 3)
             {
@@ -15,7 +17,9 @@ function searchLoop()
                 loop[i].value = loop[i].value.replace('...', '.');
             }
         }
-        else if(loop[i].innerHTML.substring(loop[i].innerHTML.length - 3) == ' ..')
+        else if(loop[i].innerHTML.substring(loop[i].innerHTML.length - 2) == ' .'
+        || loop[i].innerHTML.substring(loop[i].innerHTML.length - 3) == ' ..'
+        || loop[i].innerHTML.substring(loop[i].innerHTML.length - 4) == ' ...')
         {
             if((loop[i].innerHTML.match(/\./g) || []).length < 3)
             {
