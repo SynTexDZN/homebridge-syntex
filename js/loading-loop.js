95abb3900f2b6e7594cc631a733b1b2e14e574ca
SynTexDZN <syntexdzn@t-online.de>
2020-03-23 15:37:50 +0100

    tests

--- js/loading-loop.js
+++ js/loading-loop.js
@@ -1,41 +1,44 @@
 function searchLoop()
 {
-    setTimeout(function()
-    {
-        var loop = document.getElementsByClassName('loading-loop');
+    var loop = document.getElementsByClassName('loading-loop');
 
-        for(var i = 0; i < loop.length; i++)
+    for(var i = 0; i < loop.length; i++)
+    {
+        if(loop[i].hasAttribute('value'))
         {
-            if(loop[i].hasAttribute('value'))
+            if((loop[i].value.match(/\./g) || []).length < 3)
             {
-                if((loop[i].value.match(/\./g) || []).length < 3)
-                {
-                    loop[i].value += '.';
-                }
-                else
-                {
-                    loop[i].value = loop[i].value.replace('...', '.');
-                }
-            }
-            else if(loop[i].innerHTML.substring(loop[i].innerHTML.length - 1) == '.')
-            {
-                if((loop[i].innerHTML.match(/\./g) || []).length < 3)
-                {
-                    loop[i].innerHTML += '.';
-                }
-                else
-                {
-                    loop[i].innerHTML = loop[i].innerHTML.replace('...', '.');
-                }
+                loop[i].value += '.';
             }
             else
             {
-                console.error('Falsches Eingabeformat');
+                loop[i].value = loop[i].value.replace('...', '.');
             }
         }
+        else if(loop[i].innerHTML.substring(loop[i].innerHTML.length - 1) == ' .')
+        {
+            if((loop[i].innerHTML.match(/\./g) || []).length < 3)
+            {
+                loop[i].innerHTML += '.';
+            }
+            else
+            {
+                loop[i].innerHTML = loop[i].innerHTML.replace('...', '.');
+            }
+        }
+        else
+        {
+            console.error('Falsches Eingabeformat');
+        }
+    }
 
+    setTimeout(function()
+    {
         searchLoop();
     }, 1000);
 }
 
-searchLoop();
+setTimeout(function()
+{
+    searchLoop();
+}, 1000);
