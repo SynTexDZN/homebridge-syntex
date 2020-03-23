b015e9e96a00f9c7bb58128996e0c6fbec900c5d
SynTexDZN <syntexdzn@t-online.de>
2020-03-23 15:19:37 +0100

    tests

---
+++ js/loading-loop.js
@@ -0,0 +1,41 @@
+function searchLoop()
+{
+    setTimeout(function()
+    {
+        var loop = document.getElementsByClassName('loading-loop');
+
+        for(var i = 0; i < loop.length; i++)
+        {
+            if(loop[i].hasAttribute('value'))
+            {
+                if((loop[i].value.match(/\./g) || []).length < 3)
+                {
+                    loop[i].value += '.';
+                }
+                else
+                {
+                    loop[i].value = loop[i].value.replace('...', '.');
+                }
+            }
+            else if(loop[i].innerHTML.includes('.'))
+            {
+                if((loop[i].innerHTML.match(/\./g) || []).length < 3)
+                {
+                    loop[i].innerHTML += '.';
+                }
+                else
+                {
+                    loop[i].innerHTML = loop[i].innerHTML.replace('...', '.');
+                }
+            }
+            else
+            {
+                console.error('Falsches Eingabeformat');
+            }
+        }
+
+        searchLoop();
+    }, 1000);
+}
+
+searchLoop();
