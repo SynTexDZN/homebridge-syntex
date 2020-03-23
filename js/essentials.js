5e790073da5aa17ef19376dae263b3c2fc943732
SynTexDZN <syntexdzn@t-online.de>
2020-03-23 21:27:23 +0100

    added version comparator

---
+++ js/essentials.js
@@ -0,0 +1,20 @@
+function versionCount(version)
+{
+    var intVersion = 0;
+
+    for(var i = (version.match(/\./g) || []).length; i > 0; i--)
+    {
+        if(version.includes('-'))
+        {
+            intVersion += version.split('-')[0].split('.')[i] * Math.pow(100, i);
+        }
+        else
+        {
+            intVersion += version.split('.')[i] * Math.pow(100, i);
+        }
+
+        console.log(i, intVersion);
+    }
+
+    return intVersion;
+}
