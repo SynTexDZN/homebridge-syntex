62e3f1651faee4ced448dc35465d69b406a12224
SynTexDZN <syntexdzn@t-online.de>
2020-03-23 21:36:17 +0100

    fixes

--- js/essentials.js
+++ js/essentials.js
@@ -2,15 +2,15 @@ function versionCount(version)
 {
     var intVersion = 0;
 
-    for(var i = 0; i < (version.match(/\./g) || []).length; i++)
+    for(var i = 0; i < (version.match(/\./g) || []).length + 1; i++)
     {
         if(version.includes('-'))
         {
-            intVersion += version.split('-')[0].split('.')[i] * Math.pow(100, (version.match(/\./g) || []).length - i);
+            intVersion += version.split('-')[0].split('.')[i] * Math.pow(100, (1 + version.match(/\./g) || []).length - i);
         }
         else
         {
-            intVersion += version.split('.')[i] * Math.pow(100, (version.match(/\./g) || []).length - i);
+            intVersion += version.split('.')[i] * Math.pow(100, (1 + version.match(/\./g) || []).length - i);
         }
 
         console.log(i, intVersion);
