46f5572ba56edacc95cbf94ef633bf2b29c2cbe4
SynTexDZN <syntexdzn@t-online.de>
2020-03-24 11:49:21 +0100

    fixes

--- index.js
+++ index.js
@@ -209,7 +209,7 @@ SynTexPlatform.prototype = {
                                         if(device && !err)
                                         {    
                                             var obj = {
-                                                log: JSON.stringify(device.logs).replace(" '", ' [').replace("' ", '] ').replace(/\'\"/g, ']"')
+                                                log: JSON.stringify(device.logs).replace(/\s\'/g, ' [').replace(/\'\s/g, '] ').replace(/\'\"/g, ']"')
                                             };
                                         }
                             
