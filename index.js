56b24373a9d5581da9ab2af90f9b8f84040c724c
SynTexDZN <syntexdzn@t-online.de>
2020-08-17 23:29:09 +0200

    Added Magic Home Accessory in Main View

--- index.js
+++ index.js
@@ -469,7 +469,7 @@ SynTexPlatform.prototype = {
                                             if(magicHome.lights[i].setup == 'RGB' || magicHome.lights[i].setup == 'RGBW' || magicHome.lights[i].setup == 'RGBWW' || magicHome.lights[i].setup == 'RGBCW')
                                             {
                                                 var type = magicHome.lights[i].setup == 'RGBW' ? 'rgb' : magicHome.lights[i].setup == 'RGBWW' ? 'rgbw' :  magicHome.lights[i].setup.toLowerCase();
-                                                devices.push({ id : "-", ip : magicHome.lights[i].ip, name : magicHome.lights[i].name, type : type, version : '1.0.0', active : 1 });
+                                                devices.push({ id : "-", ip : magicHome.lights[i].ip, name : magicHome.lights[i].name, type : type, version : '99.99.99', active : 1 });
                                             }
                                         }
                                     }
