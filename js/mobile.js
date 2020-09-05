86032da284a2c77bd64705c3d3fc81b5f51b08c4
SynTexDZN <syntexdzn@t-online.de>
2020-09-05 19:17:12 +0200

    Removed Hover Effect from Touch Devices

--- js/mobile.js
+++ js/mobile.js
@@ -17,7 +17,7 @@ if(!hasTouch())
             {
                 for(var ri = styleSheet.rules.length - 1; ri >= 0; ri--)
                 {
-                    console.log(styleSheet.rules[ri].selectorText, styleSheet.rules[ri].selectorText && styleSheet.rules[ri].selectorText.includes(':hover'));
+                    console.log(styleSheet.rules[ri], styleSheet.rules[ri].selectorText && styleSheet.rules[ri].selectorText.includes(':hover'));
 
                     if(styleSheet.rules[ri].selectorText && styleSheet.rules[ri].selectorText.includes(':hover'))
                     {
@@ -27,11 +27,11 @@ if(!hasTouch())
                     {
                         for(var rj = styleSheet.rules[ri].cssRules.length - 1; rj >= 0; rj--)
                         {
-                            console.log(styleSheet.rules[ri].cssRules[rj].selectorText, styleSheet.rules[ri].cssRules[rj].selectorText && styleSheet.rules[ri].cssRules[rj].selectorText.includes(':hover'));
+                            console.log(styleSheet.rules[ri].cssRules[rj], styleSheet.rules[ri].cssRules[rj].selectorText && styleSheet.rules[ri].cssRules[rj].selectorText.includes(':hover'));
 
                             if(styleSheet.rules[ri].cssRules[rj].selectorText && styleSheet.rules[ri].cssRules[rj].selectorText.includes(':hover'))
                             {
-                                styleSheet.rules[ri].cssRules.deleteRule(ri);
+                                styleSheet.rules[ri].cssRules.deleteRule(rj);
                             }
                         }
                     }
