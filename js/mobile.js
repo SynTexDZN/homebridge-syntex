fea17441c2a6b0bbe4518b190e0edbac4a698338
SynTexDZN <syntexdzn@t-online.de>
2020-09-05 18:29:25 +0200

    Removed Hover Effect from Touch Devices

---
+++ js/mobile.js
@@ -0,0 +1,29 @@
+function hasTouch()
+{
+    return 'ontouchstart' in document.documentElement
+        || navigator.maxTouchPoints > 0
+        || navigator.msMaxTouchPoints > 0;
+}
+
+if(hasTouch())
+{
+    try
+    {
+        for(var si in document.styleSheets)
+        {
+            var styleSheet = document.styleSheets[si];
+
+            if(!styleSheet.rules) continue;
+
+            for(var ri = styleSheet.rules.length - 1; ri >= 0; ri--)
+            {
+                if(!styleSheet.rules[ri].selectorText) continue;
+
+                if(styleSheet.rules[ri].selectorText.match(':hover'))
+                {
+                    styleSheet.deleteRule(ri);
+                }
+            }
+        }
+    } catch (ex) {}
+}
