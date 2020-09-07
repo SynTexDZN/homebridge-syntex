b99e336564a4a9d9c0b3294b8dbfe73b01ac13af
SynTexDZN <syntexdzn@t-online.de>
2020-09-07 17:08:08 +0200

    Page Loading Improvements

--- js/loading.js
+++ js/loading.js
@@ -1,7 +1,7 @@
 window.addEventListener('load', function ()
 {
     document.getElementById('preloader').style.opacity = 0;
-    document.getElementById('preloader').getElementsByClassName('loader-4')[0].style.transition = '.2s';
+    document.getElementById('preloader').getElementsByClassName('loader-4')[0].style.transition = '.1s';
     document.getElementById('preloader').getElementsByClassName('loader-4')[0].style.opacity = 0;
     
     setTimeout(function()
