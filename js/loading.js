1a32fc2f3d1f1039b53e364c92e51d6531944b66
SynTexDZN <syntexdzn@t-online.de>
2020-09-07 16:48:00 +0200

    Page Loading Improvements

--- js/loading.js
+++ js/loading.js
@@ -6,7 +6,7 @@ window.addEventListener('load', function ()
     {
         //document.getElementById("preloader").parentElement.removeChild(document.getElementById("preloader"));
         
-        document.getElementById('preloader').getElementsByClassName('loader-4')[0].style.transition  = '0s';
+        document.getElementById('preloader').getElementsByClassName('loader-4')[0].style.transition = '0s';
         document.getElementById('preloader').getElementsByClassName('loader-4')[0].style.opacity = 0;
         document.getElementById('preloader').style.pointerEvents = 'none';
     }, 200);
