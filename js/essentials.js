6775790782bd17b60828c122fa60d539b0499b19
SynTexDZN <syntexdzn@t-online.de>
2020-09-07 17:06:18 +0200

    Page Loading Improvements

--- js/essentials.js
+++ js/essentials.js
@@ -315,11 +315,11 @@ async function leavePage(url)
             }
 
             document.getElementById('preloader').style.opacity = 0;
+            document.getElementById('preloader').getElementsByClassName('loader-4')[0].style.transition  = '.2s';
+            document.getElementById('preloader').getElementsByClassName('loader-4')[0].style.opacity = 0;
 
             setTimeout(function()
             {
-                document.getElementById('preloader').getElementsByClassName('loader-4')[0].style.transition  = '0s';
-                document.getElementById('preloader').getElementsByClassName('loader-4')[0].style.opacity = 0;
                 document.getElementById('preloader').style.pointerEvents = 'none';
             }, 200);
         }
