9e750eaa114f5d2533b2d98a6d1bbdce99a92456
SynTex <syntexdzn@t-online.de>
2020-11-04 17:56:57 +0100

    Improvements to Preloader Overlay

--- js/loading.js
+++ js/loading.js
@@ -1,11 +1,32 @@
+var loadFinished = false, scriptsFinished = false;
+
 window.addEventListener('load', function ()
 {
-    document.getElementById('preloader').style.opacity = 0;
-    document.getElementById('preloader').getElementsByClassName('loader-4')[0].style.transition = '.2s';
-    document.getElementById('preloader').getElementsByClassName('loader-4')[0].style.opacity = 0;
+    loadFinished = true;
     
-    setTimeout(function()
-    {
-        document.getElementById('preloader').style.pointerEvents = 'none';
-    }, 200);
+    removePreloader();
 });
+
+function finish()
+{
+    scriptsFinished = true;
+
+    removePreloader();
+}
+
+function removePreloader()
+{
+    if(scriptsFinished && loadFinished)
+    {
+        document.getElementById('preloader').style.opacity = 0;
+        document.getElementById('preloader').getElementsByClassName('loader-4')[0].style.transition = '.2s';
+        document.getElementById('preloader').getElementsByClassName('loader-4')[0].style.opacity = 0;
+        
+        setTimeout(function()
+        {
+            document.getElementById('preloader').style.pointerEvents = 'none';
+        }, 200);
+    }
+}
+
+export let Preloader = { finish };
