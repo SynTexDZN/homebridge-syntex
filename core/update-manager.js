57a1aa0ed504c90be534119652b35f790ac2ce1f
SynTex <syntexdzn@t-online.de>
2021-02-17 02:17:24 +0100

    Cleaned Up

--- core/update-manager.js
+++ core/update-manager.js
@@ -2,112 +2,112 @@ const request = require('request');
 
 module.exports = class UpdateManager
 {
-    constructor(interval, config)
-    {
-        this.config = config;
+	constructor(interval, config)
+	{
+		this.config = config;
 
-        this.newestDeviceVersions = {};
-        this.newestPluginVersions = {};
+		this.newestDeviceVersions = {};
+		this.newestPluginVersions = {};
 
-        this.fetchDeviceUpdates(interval * 1000);
-        this.fetchPluginUpdates(interval * 1000);
+		this.fetchDeviceUpdates(interval * 1000);
+		this.fetchPluginUpdates(interval * 1000);
 
-        this.updateDeviceInterval = setInterval(() => this.fetchDeviceUpdates(interval * 500), interval * 1000);
-        this.updatePluginInterval = setInterval(() => this.fetchPluginUpdates(interval * 500), interval * 1000);
-    }
+		this.updateDeviceInterval = setInterval(() => this.fetchDeviceUpdates(interval * 500), interval * 1000);
+		this.updatePluginInterval = setInterval(() => this.fetchPluginUpdates(interval * 500), interval * 1000);
+	}
 
-    fetchPluginUpdates(timeout)
-    {
-        return new Promise((callback) => {
+	fetchPluginUpdates(timeout)
+	{
+		return new Promise((callback) => {
 
-            var plugins = {};
-            var promiseArray = [];
+			var plugins = {};
+			var promiseArray = [];
 
-            this.config.load('config', (err, obj) => {    
+			this.config.load('config', (err, obj) => {    
 
-                var pl = { 'SynTex' : 'homebridge-syntex', 'SynTexWebHooks' : 'homebridge-syntex-webhooks', 'SynTexTuya' : 'homebridge-syntex-tuya', 'SynTexMagicHome' : 'homebridge-syntex-magichome' };
-    
-                if(obj && !err)
-                {       
-                    for(const p in obj.platforms)
-                    {
-                        if(obj.platforms[p].platform != null && obj.platforms[p].platform != 'config' && pl[obj.platforms[p].platform] != null)
-                        {
-                            plugins[obj.platforms[p].platform] = pl[obj.platforms[p].platform];
-                        }
-                    }
-                }
+				var pl = { 'SynTex' : 'homebridge-syntex', 'SynTexWebHooks' : 'homebridge-syntex-webhooks', 'SynTexTuya' : 'homebridge-syntex-tuya', 'SynTexMagicHome' : 'homebridge-syntex-magichome' };
+	
+				if(obj && !err)
+				{       
+					for(const p in obj.platforms)
+					{
+						if(obj.platforms[p].platform != null && obj.platforms[p].platform != 'config' && pl[obj.platforms[p].platform] != null)
+						{
+							plugins[obj.platforms[p].platform] = pl[obj.platforms[p].platform];
+						}
+					}
+				}
 
-                for(const i in plugins)
-                {
-                    var theRequest = {
-                        method : 'GET',
-                        url : 'http://registry.npmjs.org/-/package/' + plugins[i] + '/dist-tags',
-                        timeout : timeout
-                    };
+				for(const i in plugins)
+				{
+					var theRequest = {
+						method : 'GET',
+						url : 'http://registry.npmjs.org/-/package/' + plugins[i] + '/dist-tags',
+						timeout : timeout
+					};
 
-                    const newPromise = new Promise((resolve) => request(theRequest, (error, response, body) => {
+					const newPromise = new Promise((resolve) => request(theRequest, (error, response, body) => {
 
-                        try
-                        {
-                            this.newestPluginVersions[i] = JSON.parse(body);
+						try
+						{
+							this.newestPluginVersions[i] = JSON.parse(body);
 
-                            resolve(true);
-                        }
-                        catch(e)
-                        {
-                            console.error(e);
+							resolve(true);
+						}
+						catch(e)
+						{
+							console.error(e);
 
-                            resolve(false);
-                        }
-                    }));
+							resolve(false);
+						}
+					}));
 
-                    promiseArray.push(newPromise);
-                }
+					promiseArray.push(newPromise);
+				}
 
-                Promise.all(promiseArray).then(() => callback());
-            });
-        });
-    }
+				Promise.all(promiseArray).then(() => callback());
+			});
+		});
+	}
 
-    fetchDeviceUpdates(timeout)
-    {
-        return new Promise((resolve) => {
+	fetchDeviceUpdates(timeout)
+	{
+		return new Promise((resolve) => {
 
-            var theRequest = {
-                method : 'GET',
-                url : 'http://syntex.sytes.net/smarthome/check-version.php',
-                timeout : timeout
-            };
+			var theRequest = {
+				method : 'GET',
+				url : 'http://syntex.sytes.net/smarthome/check-version.php',
+				timeout : timeout
+			};
 
-            request(theRequest, (error, response, body) => {
+			request(theRequest, (error, response, body) => {
 
-                try
-                {
-                    var updates = JSON.parse(body);
+				try
+				{
+					var updates = JSON.parse(body);
 
-                    for(const update in updates)
-                    {
-                        if(!updates[update].type.startsWith('SynTex'))
-                        {
-                            this.newestDeviceVersions[updates[update].type] = updates[update].version;
-                        }
-                    }
+					for(const update in updates)
+					{
+						if(!updates[update].type.startsWith('SynTex'))
+						{
+							this.newestDeviceVersions[updates[update].type] = updates[update].version;
+						}
+					}
 
-                    resolve(true);
-                }
-                catch(e)
-                {
-                    console.error(e);
+					resolve(true);
+				}
+				catch(e)
+				{
+					console.error(e);
 
-                    resolve(false);
-                }
-            });
-        });
-    }
+					resolve(false);
+				}
+			});
+		});
+	}
 
-    getLatestVersions()
-    {
-        return { plugins : this.newestPluginVersions, devices : this.newestDeviceVersions } || { plugins : {}, devices : {} };
-    }
+	getLatestVersions()
+	{
+		return { plugins : this.newestPluginVersions, devices : this.newestDeviceVersions } || { plugins : {}, devices : {} };
+	}
 }
--- js/expandable-button.js
+++ js/expandable-button.js
@@ -3,61 +3,60 @@ var lastWidth = document.getElementsByTagName('body')[0].offsetWidth;
 
 document.getElementsByTagName('body')[0].onresize = function()
 {
-    var width = document.getElementsByTagName('body')[0].offsetWidth;
+	var width = document.getElementsByTagName('body')[0].offsetWidth;
 
-    console.log(document.getElementsByTagName('body')[0].offsetWidth, document.getElementsByTagName('body')[0].clientWidth, document.getElementsByTagName('body')[0].scrollWidth);
+	if(lastWidth < 1151 && width > 1151 || lastWidth > 1151 && width < 1151)
+	{
+		lastWidth = width;
 
-    if(lastWidth < 1151 && width > 1151 || lastWidth > 1151 && width < 1151)
-    {
-        lastWidth = width;
+		for(const i in buttons)
+		{
+			buttons[i].style.transition = 'none';
 
-        for(const i in buttons)
-        {
-            buttons[i].style.transition = 'none';
+			if(buttons[i].offsetHeight > 60)
+			{
+				buttons[i].style.removeProperty('height');
+			}
 
-            if(buttons[i].offsetHeight > 60)
-            {
-                buttons[i].style.removeProperty('height');
-            }
+			setTimeout(() => {
 
-            setTimeout(() => {
+				buttons[i].style.transition = '.3s ease-in-out height';
 
-                buttons[i].style.transition = '.3s ease-in-out height';
-
-            }, 100);
-        }
-    }
+			}, 100);
+		}
+	}
 }
 
 function createExpandableButton(btn)
 {
-    btn.onclick = function()
-    {
-        var height = btn.scrollHeight;
+	btn.onclick = function()
+	{
+		var height = btn.scrollHeight;
 
-        if(height != btn.offsetHeight)
-        {
-            btn.style.height = height;
-        }
-        else
-        {
-            btn.style.removeProperty('height');
-        }
+		if(height != btn.offsetHeight)
+		{
+			btn.style.height = height;
+		}
+		else
+		{
+			btn.style.removeProperty('height');
+		}
+		/*
+		for(const i in buttons)
+		{
+			console.log(buttons[i].parentElement.id, this.parentElement.id);
 
-        for(const i in buttons)
-        {
-            console.log(buttons[i].parentElement.id, this.parentElement.id);
+			if(buttons[i] != this)
+			{
+				buttons[i].style.removeProperty('height');
+			}
+		}
 
-            if(buttons[i] != this)
-            {
-                //buttons[i].style.removeProperty('height');
-            }
-        }
+		console.log(btn.offsetHeight, btn.clientHeight, btn.scrollHeight);
+		*/
+	};
 
-        console.log(btn.offsetHeight, btn.clientHeight, btn.scrollHeight);
-    };
-
-    buttons.push(btn);
+	buttons.push(btn);
 }
 
 export let Expandable = { createExpandableButton };
