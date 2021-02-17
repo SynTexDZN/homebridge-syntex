4d5f6e22b4e25e122391f58b44b25b8a2b7b6b07
SynTex <syntexdzn@t-online.de>
2021-02-17 01:58:02 +0100

    Expandable Button Resize Fixes

--- js/expandable-button.js
+++ js/expandable-button.js
@@ -1,5 +1,24 @@
 var buttons = [];
 
+document.getElementsByTagName('body')[0].onresize = function()
+{
+    for(const i in buttons)
+    {
+        buttons[i].style.transition = 'none';
+
+        if(buttons[i].offsetHeight > 60)
+        {
+            buttons[i].style.removeProperty('height');
+        }
+
+        setTimeout(() => {
+
+            buttons[i].style.transition = '.3s ease-in-out height';
+
+        }, 100);
+    }
+}
+
 function createExpandableButton(btn)
 {
     btn.onclick = function()
