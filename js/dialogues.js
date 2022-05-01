f3232babd0df35c6623d0a977ad98b7f5d9af4b3
SynTex <syntexdzn@t-online.de>
2022-05-01 23:55:43 +0200

    Added Dialogues Module

---
+++ js/dialogues.js
@@ -0,0 +1,61 @@
+class DialogueManager
+{
+	constructor() {}
+
+	SETUP(Query, Essentials)
+	{
+		this.Query = Query;
+		this.Essentials = Essentials;
+	}
+
+	showRestartPanel(text)
+	{
+		var dialogue = {
+			title : 'Bridge Neustarten',
+			subtitle : text,
+			buttons : [
+				{
+					text : 'Zurück',
+					close : true
+				},
+				{
+					text : 'Neustart',
+					action : async () => {
+
+						var btn = document.getElementById('dialogue').getElementsByClassName('button-area')[0].children[1].children[0];
+
+						var overlays = {
+							root : btn,
+							id : 'restart',
+							pending : { value : '%general.restarting% ..' },
+							executeError : { value : '%general.restart_failed%!' },
+							connectionError : { value : '%general.bridge_connection_error%!' }
+						};
+
+						if(await this.Query.complexFetch('/serverside/restart', 5000, 2, overlays, true) == 'Success')
+						{
+							await this.Essentials.newTimeout(200);
+
+							if(await this.Essentials.checkRestart('/serverside/check-restart'))
+							{
+								this.Essentials.showOverlay(btn, this.Essentials.createSuccessOverlay('restart-result', '%general.restart_success%!'));
+							}
+							else
+							{
+								this.Essentials.showOverlay(btn, this.Essentials.createErrorOverlay('restart-result', '%general.restart_failed%!'));
+							}
+
+							await this.Essentials.newTimeout(2000);
+
+							this.Essentials.closeDialogue();
+						}
+					}
+				}
+			]
+		};
+
+		this.Essentials.openDialogue(dialogue);
+	}
+}
+
+export let Dialogues = new DialogueManager();
