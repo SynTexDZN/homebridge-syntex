7077cc1064fc80957c72575c95767291cbc97827
SynTex <syntexdzn@t-online.de>
2024-09-14 23:00:06 +0200

    Added More Functionality

---
+++ core/automation/socket-manager.js
@@ -0,0 +1,39 @@
+module.exports = class SocketManager
+{
+    constructor(platform)
+    {
+        this.sockets = [];
+    }
+
+    addSocket(socket, id, letters)
+    {
+        var found = false;
+
+		for(const client of this.sockets)
+		{
+			if(client.socket == socket)
+			{
+				client.id = id;
+				client.letters = letters;
+
+				found = true;
+			}
+		}
+
+		if(!found)
+		{
+			this.sockets.push({ socket, id, letters });
+		}
+    }
+
+    updateSockets(id, letters, message)
+    {
+        for(const client of this.sockets)
+        {
+            if(client.id == id && client.letters == letters)
+            {
+                client.socket.send(JSON.stringify(message));
+            }
+        }
+    }
+}
--- core/automation/state-manager.js
+++ core/automation/state-manager.js
@@ -1,4 +1,4 @@
-const AutomationSystem = require('./automation-system');
+const AutomationSystem = require('./automation-system'), SocketManager = require('./socket-manager');
 
 module.exports = class StateManager
 {
@@ -12,8 +12,12 @@ module.exports = class StateManager
         this.logger = platform.logger;
 
         this.EventManager = platform.EventManager;
+        this.WebServer = platform.WebServer;
+
+        this.HistoryManager = new HistoryManager(this);
 
         this.AutomationSystem = new AutomationSystem(platform);
+        this.SocketManager = new SocketManager(platform);
 
         this.initSocket();
 
@@ -42,23 +46,26 @@ module.exports = class StateManager
                 {
                     for(const y in data[x])
                     {
-                        for(const z in data[x][y])
+                        if(data[x][y].state != null)
                         {
-                            if(this.data[x] == null)
+                            for(const z in data[x][y].state)
                             {
-                                this.data[x] = {};
-                            }
-
-                            if(this.data[x][y] == null)
-                            {
-                                this.data[x][y] = {};
-                            }
-
-                            if(this.data[x][y][z] == null)
-                            {
-                                this.data[x][y][z] = data[x][y][z];
-                            }
-                        }  
+                                if(this.data[x] == null)
+                                {
+                                    this.data[x] = {};
+                                }
+    
+                                if(this.data[x][y] == null)
+                                {
+                                    this.data[x][y] = { state : {} };
+                                }
+    
+                                if(this.data[x][y].state[z] == null)
+                                {
+                                    this.data[x][y].state[z] = data[x][y].state[z];
+                                }
+                            }  
+                        }
                     }
                 }
 
@@ -73,31 +80,137 @@ module.exports = class StateManager
 
 			if(message.service != null && message.state != null)
 			{
-				if(this.data[message.service.id] == null)
-				{
-					this.data[message.service.id] = {};
-				}
-
-				if(this.data[message.service.id][message.service.letters] == null)
-				{
-					this.data[message.service.id][message.service.letters] = {};
-				}
-
-				for(const x in message.state)
-				{
-                    if(this.data[message.service.id][message.service.letters][x] != message.state[x])
-                    {
-                        this.changed = true;
-                    }
-
-					this.data[message.service.id][message.service.letters][x] = message.state[x];
-				}
-
-				if(message.type == 'WRITE')
-				{
-					this.AutomationSystem.runAutomation(message.service, this.data[message.service.id][message.service.letters]);
-				}
+				this.updateState(message.service.id, message.service.letters, message.service.name, message.state, message.type);
 			}
 		});
+
+        this.WebServer.addSocket('/devices', 'getState', (ws, params) => {
+
+            var response = { state : {}, history : [], automation : [] };
+
+            if(this.data[params.id] != null && this.data[params.id][params.letters] != null)
+            {
+                response = this.data[params.id][params.letters];
+            }
+
+            this.SocketManager.addSocket(ws, params.id, params.letters);
+
+            ws.send(JSON.stringify(response));
+        });
+    }
+
+    updateState(id, letters, name, state, type)
+    {
+        this._prepareStructure(id, letters);
+
+        if(this._hasStateChanged(id, letters, state))
+        {
+            for(const x in state)
+            {
+                this.data[id][letters].state[x] = state[x];
+            }
+
+            if(type == 'WRITE')
+            {
+                this.data[id][letters].time = new Date().getTime();
+
+                this.HistoryManager.updateCycle(id, letters, this.data[id][letters].state);
+
+                this.AutomationSystem.runAutomation({ id, letters, name }, this.data[id][letters].state);
+            }
+
+            this.SocketManager.updateSockets(id, letters, this.data[id][letters]);
+
+            this.changed = true;
+        }
+    }
+
+    _prepareStructure(id, letters)
+    {
+        if(this.data[id] == null)
+        {
+            this.data[id] = {};
+        }
+
+        if(this.data[id][letters] == null)
+        {
+            this.data[id][letters] = { state : {}, history : [], automation : [] };
+        }
+    }
+
+    _hasStateChanged(id, letters, state)
+    {
+        for(const x in state)
+        {
+            if(this.data[id][letters].state[x] != state[x])
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
+
+class HistoryManager
+{
+    constructor(StateManager)
+    {
+        this.data = StateManager.data;
+    }
+
+    updateCycle(id, letters, state)
+    {
+        var time = this.getCurrentCycle(), latestCycle = this.getCycle(id, letters, time);
+
+        if(latestCycle == null)
+        {
+            latestCycle = { time, state : {}};
+
+            this.data[id][letters].history.push(latestCycle);
+        }
+
+        for(const x in state)
+        {
+            if(latestCycle.state[x] == null)
+            {
+                latestCycle.state[x] = {};
+            }
+
+            latestCycle.state[x].latest = state[x];
+
+            if(latestCycle.state[x].min == null || latestCycle.state[x].min < state[x])
+            {
+                latestCycle.state[x].min = state[x];
+            }
+
+            if(latestCycle.state[x].max == null || latestCycle.state[x].max > state[x])
+            {
+                latestCycle.state[x].max = state[x];
+            }
+        }
+    }
+
+    getCurrentCycle()
+    {
+        var date = new Date(new Date().getTime());
+
+		date.setSeconds(0);
+		date.setMilliseconds(0);
+
+		return date.getTime();
+    }
+
+    getCycle(id, letters, time)
+    {
+        for(const entry of this.data[id][letters].history)
+        {
+            if(entry.time == time)
+            {
+                return entry;
+            }
+        }
+
+        return null;
     }
 }
