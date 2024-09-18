d3057f0a116ce763ce7fbaa62fdf9e75524288af
SynTex <syntexdzn@t-online.de>
2024-09-18 22:36:44 +0200

    Store State Events

--- core/automation/activity-manager.js
+++ core/automation/activity-manager.js
@@ -4,6 +4,8 @@ module.exports = class ActivityManager
 {
 	constructor(platform)
 	{
+		this.data = {};
+
 		this.EventManager = platform.EventManager;
 		this.RequestManager = platform.RequestManager;
 
@@ -16,6 +18,14 @@ module.exports = class ActivityManager
 
 	initSocket()
 	{
+		this.EventManager.setInputStream('stateUpdate', { source : this, external : true }, (message) => {
+
+			if(message.service != null && message.state != null)
+			{
+				this.updateState(message.service.id, message.service.letters, message.state);
+			}
+		});
+
 		this.EventManager.setInputStream('runAutomation', { source : this, external : true }, (message) => {
 
 			if(message.service != null && message.state != null)
@@ -23,6 +33,16 @@ module.exports = class ActivityManager
 				this.AutomationSystem.runAutomation(message.service, message.state);
 			}
 		});
+	}
+
+	updateState(id, letters, state)
+	{
+		this._prepareStructure(id, letters);
+
+		for(const x in state)
+		{
+			this.data[id][letters].state[x] = state[x];
+		}
 	}
 
 	updateAutomation(id, letters, automation)
@@ -48,5 +68,32 @@ module.exports = class ActivityManager
 				resolve(null);
 			}
 		});
+	}
+	_prepareStructure(id, letters)
+	{
+		if(this.data[id] == null)
+		{
+			this.data[id] = {};
+		}
+
+		if(this.data[id][letters] == null)
+		{
+			this.data[id][letters] = {};
+		}
+
+		if(this.data[id][letters].state == null)
+		{
+			this.data[id][letters].state = {};
+		}
+
+		if(this.data[id][letters].history == null)
+		{
+			this.data[id][letters].history = [];
+		}
+
+		if(this.data[id][letters].automation == null)
+		{
+			this.data[id][letters].automation = [];
+		}
 	}
 }
