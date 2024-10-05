13cf06fcb7d6206c2e99aed711adcb1dd6ec5717
SynTex <syntexdzn@t-online.de>
2024-10-05 23:22:57 +0200

    Added History Manager

--- core/automation/activity-manager.js
+++ core/automation/activity-manager.js
@@ -1,10 +1,11 @@
-const AutomationSystem = require('./automation-system'), RouteManager = require('./route-manager'), TypeManager = require('./type-manager');;
+const AutomationSystem = require('./automation-system'), RouteManager = require('./route-manager'), TypeManager = require('./type-manager');
 
 module.exports = class ActivityManager
 {
 	constructor(platform)
 	{
 		this.data = {};
+		this.history = {};
 
 		this.logger = platform.logger;
 
@@ -14,6 +15,7 @@ module.exports = class ActivityManager
 		this.TypeManager = new TypeManager(platform);
 		this.RouteManager = new RouteManager(platform);
 		this.AutomationSystem = new AutomationSystem(platform, this);
+		this.HistoryManager = new HistoryManager(platform, this);
 
 		this.initSocket();
 	}
@@ -24,7 +26,7 @@ module.exports = class ActivityManager
 
 			if(message.service != null && message.state != null)
 			{
-				this.updateState(message.service.id, message.service.letters, message.state);
+				this.updateState(message.service.id, message.service.letters, message.state, message.type);
 			}
 		});
 
@@ -37,13 +39,21 @@ module.exports = class ActivityManager
 		});
 	}
 
-	updateState(id, letters, state)
+	updateState(id, letters, state, type)
 	{
 		this._prepareStructure(id, letters);
 
-		for(const x in state)
+		if(this._hasStateChanged(id, letters, state))
 		{
-			this.data[id][letters][x] = state[x];
+			for(const x in state)
+			{
+				this.data[id][letters][x] = state[x];
+			}
+
+			if(type == 'WRITE')
+			{
+				this.HistoryManager.updateCycle(id, letters, this.data[id][letters]);
+			}
 		}
 	}
 
@@ -120,6 +130,148 @@ module.exports = class ActivityManager
 		if(this.data[id][letters] == null)
 		{
 			this.data[id][letters] = {};
+		}
+	}
+
+	_hasStateChanged(id, letters, state)
+	{
+		for(const x in state)
+		{
+			if(this.data[id][letters][x] != state[x])
+			{
+				return true;
+			}
+		}
+
+		return false;
+	}
+}
+
+class HistoryManager
+{
+	constructor(platform, StateManager)
+	{
+		this.changed = false;
+
+		this.files = platform.files;
+
+		this.data = StateManager.history;
+
+		this.loadData().then(() => {
+
+			setInterval(() => {
+
+				if(this.changed)
+				{
+					this.files.writeFile('cache/history.json', this.data);
+
+					this.changed = false;
+				}
+
+			}, 10000);
+		});
+	}
+
+	loadData()
+	{
+		return new Promise((resolve) => {
+
+			this.files.readFile('cache/history.json').then((data) => {
+
+				for(const x in data)
+				{
+					for(const y in data[x])
+					{
+						this._prepareStructure(x, y);
+						
+						if(data[x][y].time != null)
+						{
+							this.data[x][y].time = data[x][y].time;
+						}
+
+						if(data[x][y].history != null)
+						{
+							this.data[x][y].history = data[x][y].history;
+						}
+					}
+				}
+
+				resolve();
+			});
+		});
+	}
+
+	updateCycle(id, letters, state)
+	{
+		this._prepareStructure(id, letters);
+		
+		var time = this.getCurrentCycle(), latestCycle = this.getCycle(id, letters, time);
+
+		this.data[id][letters].time = new Date().getTime();
+
+		if(latestCycle == null)
+		{
+			latestCycle = { time, state : {}};
+
+			this.data[id][letters].history.push(latestCycle);
+		}
+
+		for(const x in state)
+		{
+			if(latestCycle.state[x] == null)
+			{
+				latestCycle.state[x] = {};
+			}
+
+			latestCycle.state[x].latest = state[x];
+
+			if(latestCycle.state[x].min == null || latestCycle.state[x].min < state[x])
+			{
+				latestCycle.state[x].min = state[x];
+			}
+
+			if(latestCycle.state[x].max == null || latestCycle.state[x].max > state[x])
+			{
+				latestCycle.state[x].max = state[x];
+			}
+		}
+
+		this.changed = true;
+	}
+
+	getCurrentCycle()
+	{
+		var date = new Date(new Date().getTime());
+
+		date.setSeconds(0);
+		date.setMilliseconds(0);
+
+		return date.getTime();
+	}
+
+	getCycle(id, letters, time)
+	{
+		for(const entry of this.data[id][letters].history)
+		{
+			if(entry.time == time)
+			{
+				return entry;
+			}
+		}
+
+		return null;
+	}
+
+	_prepareStructure(id, letters)
+	{
+		if(this.data[id] == null)
+		{
+			this.data[id] = {};
+		}
+
+		if(this.data[id][letters] == null)
+		{
+			this.data[id][letters] = { history : [] };
 		}
 	}
 }
