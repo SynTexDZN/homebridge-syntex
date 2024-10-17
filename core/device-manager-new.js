fcd1c19c372420d139986580a436fca9270c5061
SynTex <syntexdzn@t-online.de>
2024-10-18 00:32:31 +0200

    Added State From ActivityManager Added AID From Bridge Accessories

--- core/device-manager-new.js
+++ core/device-manager-new.js
@@ -12,6 +12,7 @@ module.exports = class DeviceManager
 		this.files = platform.files;
 
 		this.ActivityManager = platform.ActivityManager;
+		this.RequestManager = platform.RequestManager;
 		this.WebServer = platform.WebServer;
 
 		this.loadAccessories();
@@ -21,6 +22,50 @@ module.exports = class DeviceManager
 
 	loadAccessories()
 	{
+		const readBridge = () => {
+
+			return new Promise((resolve) => {
+
+				this.RequestManager.fetch('http://localhost:51826/accessories', { verbose : true }).then((response) => {
+
+					var accessories = [];
+
+					if(response.data instanceof Object && response.data.accessories != null)
+					{
+						for(const i in response.data.accessories)
+						{
+							var accessory = {}, isBridge = false;
+	
+							accessory.aid = response.data.accessories[i].aid;
+	
+							for(const j in response.data.accessories[i].services)
+							{
+								if(response.data.accessories[i].services[j].type == 'A2')
+								{
+									isBridge = true;
+								}
+								
+								for(const k in response.data.accessories[i].services[j].characteristics)
+								{
+									if(response.data.accessories[i].services[j].type == '3E' && response.data.accessories[i].services[j].characteristics[k].type == '30')
+									{
+										accessory.id = response.data.accessories[i].services[j].characteristics[k].value;
+									}
+								}
+							}
+	
+							if(!isBridge)
+							{
+								accessories.push(accessory);
+							}
+						}
+					}
+
+					resolve(accessories);
+				});
+			});
+		};
+
 		const readConfig = () => {
 
 			return new Promise((resolve) => {
@@ -55,6 +100,14 @@ module.exports = class DeviceManager
 									}
 		
 									accessory.services = this.addLetters(accessory.services);
+
+									for(const service of accessory.services)
+									{
+										this.ActivityManager._getState({ id : accessory.id, letters : service.letters }).then((state) => {
+					
+											service.state = state;
+										});
+									}
 		
 									accessory.plugin = { alias : platform.platform };
 		
@@ -71,10 +124,24 @@ module.exports = class DeviceManager
 			});
 		};
 
-		readConfig().then((data) => {
+		readBridge().then((bridgeAccessories) => readConfig().then((configAccessories) => {
+			
+			for(const bridgeAccessory of bridgeAccessories)
+			{
+				for(const configAccessory of configAccessories)
+				{
+					if(bridgeAccessory.id == configAccessory.id)
+					{
+						for(const x in configAccessory)
+						{
+							bridgeAccessory[x] = configAccessory[x];
+						}
+					}
+				}
+			}
 
-			this.data = data;
-		});
+			this.data = bridgeAccessories;
+		}));
 	}
 
 	initWebServer()
@@ -83,6 +150,11 @@ module.exports = class DeviceManager
 
 			response.end(JSON.stringify(this.data));
 		});
+	}
+
+	getAccessories()
+	{
+		return this.data;
 	}
 
 	addLetters(services)
